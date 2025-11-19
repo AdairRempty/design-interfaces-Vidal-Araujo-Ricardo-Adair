0dfb56eb83074ca701b85498db8d22a5f5476e6a
Rempty Adair <ricardoadairva@gmail.com>
2025-11-18 21:43:06 -0600

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+import { createApp } from  'vue' ; 
+import  App  from  './App.vue' ; 
+
+createApp ( App ) .mount ( '#app' );
+app. component ( 'example-component' , ExampleComponent ) 
+app. mount ( '#app' );
